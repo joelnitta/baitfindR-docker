d0ecccfefcdc2483dd4277183d5fc1980fe4fb8c
Joel Nitta <joelnitta@gmail.com>
2019-05-16 12:54:18 -0400

    Update README

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-Dockerfile for building images to run [baitfindR](https://joelnitta.github.io/baitfindR/) and its dependencies. Based off of [rocker](https://hub.docker.com/u/rocker/).
+Dockerfile for building images to run [baitfindR](https://joelnitta.github.io/baitfindR/) and its dependencies. Based on [rocker/tidyverse](https://hub.docker.com/r/rocker/tidyverse).
+
+During the build, R packages are installed using `packrat::restore()`. For a more detailed explanation of this process, see [this blog post](https://www.joelnitta.com/post/docker-and-packrat/) and [this example repo](https://github.com/joelnitta/docker-packrat-example).
+
+The `packrat.lock` file should be built with the following command, which sources `install_packages.R` within a `rocker/tidyverse` container (change path on LHS of `-v` as needed).
+
+```
+docker run --rm -e DISABLE_AUTH=true -v /Users/joelnitta/repos/baitfindr-docker:/home/rstudio/project -w /home/rstudio/project rocker/tidyverse:3.6.0 Rscript install_packages.R
+```
--- install_packages.R
+++ install_packages.R
@@ -1,45 +1,5 @@
-#' # Installing packages to the Docker image with packrat
-#'
-#' ## How this works
-#'
-#' ### Setup
-#'
-#' Keep `Dockerfile` and `install_packages.R` in the root of the
-#' project directory.
-#'
-#' ### First step: make packrat/packrat.lock file
-#'
-#' Launch rocker image with tag set to same version we will use in
-#' the Dockerfile and this directory mounted, and run this script.
-#' (change path to this project directory if needed)
-#'
-#' ````
-#' docker run -it -e DISABLE_AUTH=true -v /Users/joelnitta/repos/baitfindR-docker/:/home/rstudio/project rocker/tidyverse:3.6.0 bash
-#' ````
-#'
-#' Install R packages.
-#' ```
-#' cd home/rstudio/project
-#' Rscript install_packages.R
-#' ```
-#'
-#' This will install current versions of all packages and deps
-#' used in this project,
-#' but the main reason is to write `packrat/packrat.lock`
-#'
-#' ### Second step: actually build the image
-#'
-#' Now we can use packrat.lock to restore (i.e., install) packages
-#' during the next (real) docker build, using the `Dockerfile`.
-#'
-#' ```
-#' docker build . -t joelnitta/baitfindr
-#' ```
-#'
-#' ### Third step: rinse, repeat
-#'
-#' Edit the packages below as needed to add new packages
-#' or update old ones (by installing), and repeat Steps 1 and 2.
+# Install packages within docker container, then write a snapshot to
+# packrat/packrat.lock.
 
 ### Initialize packrat ###
 
@@ -86,11 +46,7 @@ cran_packages <- c(
 install.packages(cran_packages)
 
 # Install github packages
-github_packages <- c(
-  "joelnitta/baitfindR"
-)
-
-remotes::install_github(github_packages)
+remotes::install_github("joelnitta/baitfindR")
 
 ### Take snapshot ###
 
